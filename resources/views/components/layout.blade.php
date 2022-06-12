80cb6a262adf830a40f100d3e15b83b67c763c89
tauraspaj <pajuodis.tauras@gmail.com>
2022-06-12 12:15:38 +0100

    Introduce blade components

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <title>{{ $title }}</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"">
+</head>
+<body class="font-barlow">
+    
+    {{ $content }}
+
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot name="title">Custom title</x-slot>
+    <x-slot name="content">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora corporis obcaecati dolorum. Eaque laudantium sit molestiae, a illo tempore aliquid. Cupiditate exercitationem repellendus voluptatum velit explicabo quos nobis tenetur architecto!
+    </x-slot>
+</x-layout>
