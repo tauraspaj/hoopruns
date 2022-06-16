c36c42f2182f4865fa1740b5a87f3c7c3668b326
tauraspaj <pajuodis.tauras@gmail.com>
2022-06-16 21:28:56 +0100

    Include fontawesome icons

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    {{-- Fontawesome icons --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body class="font-barlow bg-gray-100 text-gray-900">
 
     <div class="sticky top-0 shadow">
-
         {{-- Latest scores from latest-scores.blade.php component --}}
         {{-- $latestScoresVisibility can be set to hidden to hide it if needed --}}
         <x-latest-scores latestScoresVisibility="{{ $latestScoresVisibility }}"></x-latest-scores>
