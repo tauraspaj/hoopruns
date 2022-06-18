83a058a1e5af5eef0916975f89d3c0ec30aa4d82
tauraspaj <pajuodis.tauras@gmail.com>
2022-06-18 12:01:29 +0100

    Utilise new container class

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,14 @@
 <nav class="h-12 bg-neutral-900">
     {{-- Container wrapper --}}
-    <div class="container h-full mx-auto flex justify-between items-center text-gray-50">
+    <div class="container h-full flex justify-between items-center text-gray-50">
         {{-- Logo --}}
-        <div class="flex items-center space-x-2 px-2">
+        <div class="flex items-center space-x-2">
             <i class="fa-solid fa-basketball text-red-600"></i>
             <p class="font-bebas text-lg pt-1 tracking-wider">SURREY BASKETBALL</p>
         </div>
 
         {{-- Hamburger --}}
-        <button class="mx-2 md:hidden w-8 h-8 border rounded border-gray-400 p-1 flex flex-col justify-center items-center space-y-1 transition hover:border-orange-600 hover:text-orange-600">
+        <button class="md:hidden w-8 h-8 border rounded border-gray-400 p-1 flex flex-col justify-center items-center space-y-1 transition hover:border-orange-600 hover:text-orange-600">
             {{--  --}}
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </button>
@@ -23,7 +23,7 @@
     </div>
 
     {{-- Mobile dropdown --}}
-    <div class="md:hidden">
+    <div class="hidden md:hidden">
         <div class="bg-neutral-800 shadow text-gray-50 py-6">
             <div class="flex flex-col justify-center items-center space-y-6">
                 <a href="/" class="font-bebas text-lg tracking-wide text-red-600 transition hover:text-red-600" title="Home">Home</a>
@@ -40,6 +40,8 @@
                 </div>
             </div>
         </div>
+
+        {{-- Black curtain overlay --}}
         <div class="h-screen w-screen bg-black absolute bg-opacity-70"></div>
     </div>
 </nav>
