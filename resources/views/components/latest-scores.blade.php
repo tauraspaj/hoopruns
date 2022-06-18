6275fa7e4bd0f6ec4f428fdfc61c7c2f78ba26a6
tauraspaj <pajuodis.tauras@gmail.com>
2022-06-18 11:59:11 +0100

    Utilise new container class

--- resources/views/components/latest-scores.blade.php
+++ resources/views/components/latest-scores.blade.php
@@ -1,9 +1,9 @@
 
-<section class="bg-gray-200 hidden sm:block {{$latestScoresVisibility}}">
+<section class="bg-gray-200 text-gray-900 hidden sm:block {{$latestScoresVisibility}}">
     {{-- Container wrapper --}}
-    <div class="flex items-center container mx-auto h-full overflow-x-auto">
+    <div class="flex items-center container h-full overflow-x-auto">
         {{-- Tournament select --}}
-        <div class="flex-none border-r border-gray-300 h-full flex items-center px-2">
+        <div class="flex-none border-r border-gray-300 h-full flex items-center">
             <button class="border rounded-full bg-gray-50 px-4 py-1 flex items-center space-x-2 transition hover:bg-white">
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
                 <p class="font-semibold text-sm uppercase">World Cup Champ</p>
