92518970e9d1b2b11428235c32b9de07f8e341f7
tauraspaj <pajuodis.tauras@gmail.com>
2022-06-19 11:16:11 +0100

    Register tournaments route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/tournaments', function () {
+    return view('tournaments');
+});
