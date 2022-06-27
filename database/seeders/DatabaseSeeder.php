f8f225a10457c00b269af4bcbea54eb3035d090d
tauraspaj <pajuodis.tauras@gmail.com>
2022-06-27 20:12:04 +0100

    Tournament Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tournament;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Tournament::truncate();
+        
+        Tournament::create([
+            'title' => 'World Championship',
+            'location' => 'Guildford, Surrey Sports Park',
+            'slug' => 'world-championship',
+            'access' => 'Public',
+            'team_limit' => 8,
+            'time' => Carbon::create('2022', '7', '16', '13')
+        ]);
+
+        Tournament::create([
+            'title' => 'TALEX Cup Invitational #1',
+            'location' => 'Guildford, Surrey Sports Park',
+            'slug' => 'talex-cup-invitational-1',
+            'access' => 'Public',
+            'team_limit' => 6,
+            'time' => Carbon::create('2022', '7', '30', '13')
+        ]);
     }
 }
