2531cf7e17956b35919e1e7c589662b4e20bfd4e
tauraspaj <pajuodis.tauras@gmail.com>
2022-08-01 12:22:27 +0100

    Mobile friendly header

--- resources/views/components/index/hero-banner.blade.php
+++ resources/views/components/index/hero-banner.blade.php
@@ -1,13 +1,13 @@
-<section class="h-screen relative">
+<section class="h-screen relative overflow-x-hidden">
     {{-- Floating text behind the images --}}
     <div class="inset-0 absolute flex justify-center items-center overflow-hidden z-[-1]">
         <p class="text-gray-50 font-oswald text-center font-bold text-[15vw] whitespace-nowrap leading-[1.1] tracking-wide">RUNS<br>FOR HOOPERS</p>
     </div>
 
     {{-- Floating images --}}
-    <div class="flex absolute -bottom-28 right-20">
+    <div class="hidden md:flex scale-50 absolute -right-32 -bottom-56 lg:-bottom-40 lg:-right-16 xl:-bottom-32 xl:-right-12 lg:scale-75 2xl:scale-90 2xl:-bottom-32 2xl:right-12">
         {{-- Floating Kobe image --}}
-        <div class="mt-20 -mr-60">
+        <div class="hidden xl:block mt-20 -mr-60">
             <img src="{{ asset('images/kobe.png') }}" alt="Floating Kobe">
         </div>
     
@@ -22,24 +22,24 @@
         {{-- Floating pattern image --}}
         <img src="{{ asset('images/dot.png') }}" class="scale-75">
     </div>
-    <div class="absolute scale-50 -right-32 -bottom-64 z-[-1] opacity-20">
+    <div class="hidden xl:block absolute scale-50 -right-32 -bottom-64 z-[-1] opacity-20">
         {{-- Floating pattern image --}}
         <img src="{{ asset('images/dot.png') }}" class="scale-75">
     </div>
 
     {{-- Text --}}
-    <div class="h-full flex items-center">
+    <div class="h-full flex items-center md:-mt-16 lg:mt-0 text-center md:text-left">
         <div class="container">
             <p class="text-xs tracking-widest uppercase text-gray-900 opacity-40 font-medium">For Hoopers By Hoopers</p>
             <h1 class="font-oswald text-gray-900 font-bold tracking-wide text-8xl -ml-1">FIND A RUN</h1>
 
             <h2 class="text-gray-500 mt-6 text-lg">We organise professional basketball events for all levels of players.</h2>
-            <p class="text-gray-500 text-lg">Get in touch now to register your interest!</p>
+            <p class="text-gray-500 mt-2 text-lg">Get in touch now to register your interest!</p>
 
             {{-- Call to action buttons --}}
-            <div class="flex space-x-6 mt-12">
-                <button class="uppercase bg-gray-800 py-3 px-8 text-gray-50 font-oswald font-medium transition hover:bg-gray-900" title="Go to Tournaments">TOURNAMENTS</button>
-                <button class="uppercase border-2 border-gray-300 text-gray-900 py-3 px-8 font-oswald font-medium transition hover:border-gray-900" title="Go to Contacts Page">CONTACT US</button>
+            <div class="flex space-x-6 mt-12 justify-center md:justify-start">
+                <a href="/tournaments" class="uppercase bg-gray-800 py-3 px-8 text-gray-50 font-oswald font-medium transition hover:bg-gray-900" title="Go to Tournaments">TOURNAMENTS</a>
+                <a href="/register" class="uppercase border-2 border-gray-300 text-gray-800 py-3 px-8 font-oswald font-medium transition hover:border-gray-900" title="Go to Contacts Page">CONTACT US</a>
             </div>
         </div>
     </div>
