7fdafdc3ecd189bf8afeaf99053582c0718b947f
tauraspaj <pajuodis.tauras@gmail.com>
2022-08-01 20:19:02 +0100

    Statistics component

---
+++ resources/views/components/index/statistics.blade.php
@@ -0,0 +1,16 @@
+<section class="bg-gray-900 py-8">
+    <div class="flex flex-col md:flex-row container space-y-12 md:space-y-0">    
+        <div class="flex-1 flex justify-center items-center flex-col space-y-1">
+            <h1 class="text-4xl font-medium font-oswald">1</h1>
+            <p class="uppercase">Successful Tournaments</p>
+        </div>
+        <div class="flex-1 flex justify-center items-center flex-col space-y-1">
+            <h1 class="text-4xl font-medium font-oswald">8</h1>
+            <p class="uppercase">Teams Attended</p>
+        </div>
+        <div class="flex-1 flex justify-center items-center flex-col space-y-1">
+            <h1 class="text-4xl font-medium font-oswald">100%</h1>
+            <p class="uppercase">Happy Players</p>
+        </div>
+    </div>
+</section>
